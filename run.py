e1987d2a34fbbfcae2394c9daa53cab65280a6e2
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-04-08 18:47:32 +0200

    window implemented

---
+++ run.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    import sys
+
+    import mainwindowcontroller
+
+    sys.exit(mainwindowcontroller.run())
+
+# def run():
+#     app = QtWidgets.QApplication([])
+#     MainWindow = QtWidgets.QMainWindow()
+#     ui = Ui_MainWindow()
+#     ui.setupUi(MainWindow)
+#     MainWindow.show()
+#     app.exec_()
