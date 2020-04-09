d64b9bc300ba085c319bbe06f7855d4343cdcb18
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-04-09 13:52:27 +0200

    WM functions

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
 if __name__ == '__main__':
     import sys
-
     import mainwindowcontroller
 
     sys.exit(mainwindowcontroller.run())
-
-# def run():
-#     app = QtWidgets.QApplication([])
-#     MainWindow = QtWidgets.QMainWindow()
-#     ui = Ui_MainWindow()
-#     ui.setupUi(MainWindow)
-#     MainWindow.show()
-#     app.exec_()
