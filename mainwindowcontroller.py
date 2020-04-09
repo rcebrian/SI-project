e150b79397a09a671437ff95bcc57ed70a809dfc
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-04-09 20:06:20 +0200

    scraper view finished

--- mainwindowcontroller.py
+++ mainwindowcontroller.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtWidgets
 
-from analysis import utils as analysis
+from analysis import utils as la
 from mainwindow import Ui_MainWindow
+from scrapers import scrapers as scr
 
 
 class MainController(QtWidgets.QMainWindow):
@@ -12,7 +13,71 @@ class MainController(QtWidgets.QMainWindow):
         super(MainController, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.btn_make_analysis.clicked.connect(analysis.pre_process_all_files)  # btn to tokenize all files
+        self.ui.btn_make_analysis.clicked.connect(la.pre_process_all_files)  # btn to tokenize all files
+
+        # Tab scrapers
+        self.ui.btn_scrapers_20m_run.clicked.connect(self.scraper_20m)
+        self.ui.btn_scrapers_elPais_run.clicked.connect(self.scraper_elPais)
+        self.ui.btn_scrapers_elMundo_run.clicked.connect(self.scraper_elMundo)
+        self.ui.btn_scrapers_run_all.clicked.connect(self.scraper_run_all)
+        self.ui.btn_scrapers_refresh_sta.clicked.connect(self.refresh)
+        self.ui.btn_scrapers_refresh_sta.clicked
+
+    def get_categories(self, opt):
+        categories = []
+        if opt is 0:
+            categories.append('salud')
+            categories.append('ciencia')
+            categories.append('tecnologia')
+        elif opt is 1:
+            categories.append('salud')
+        elif opt is 2:
+            categories.append('ciencia')
+        elif opt is 3:
+            categories.append('tecnologia')
+        return categories
+
+    def scraper_20m(self):
+        scr.scraper_20minutos(self.get_categories(self.ui.cb_scrapers_20m.currentIndex()))
+        self.refresh()
+
+    def scraper_elMundo(self):
+        scr.scraper_elMundo(self.get_categories(self.ui.cb_scrapers_elMundo.currentIndex()))
+        self.refresh()
+
+    def scraper_elPais(self):
+        scr.scraper_elPais(self.get_categories(self.ui.cb_scrapers_elPais.currentIndex()))
+        self.refresh()
+
+    def scraper_run_all(self):
+        self.scraper_20m()
+        self.scraper_elMundo()
+        self.scraper_elPais()
+
+    def refresh(self):
+        self.ui.lbl_scrapers_20m_health_result.setNum(scr.count_files('20Minutos', 'salud'))
+        self.ui.lbl_scrapers_20m_science_result.setNum(scr.count_files('20Minutos', 'ciencia'))
+        self.ui.lbl_scrapers_20m_tech_result.setNum(scr.count_files('20Minutos', 'tecnologia'))
+        self.ui.lbl_scrapers_20m_total_result.setNum(
+            int(self.ui.lbl_scrapers_20m_health_result.text()) +
+            int(self.ui.lbl_scrapers_20m_science_result.text()) +
+            int(self.ui.lbl_scrapers_20m_tech_result.text()))
+
+        self.ui.lbl_scrapers_elMundo_health_result.setNum(scr.count_files('elMundo', 'salud'))
+        self.ui.lbl_scrapers_elMundo_science_result.setNum(scr.count_files('elMundo', 'ciencia'))
+        self.ui.lbl_scrapers_elMundo_tech_result.setNum(scr.count_files('elMundo', 'tecnologia'))
+        self.ui.lbl_scrapers_elMundo_total_result.setNum(
+            int(self.ui.lbl_scrapers_elMundo_health_result.text()) +
+            int(self.ui.lbl_scrapers_elMundo_science_result.text()) +
+            int(self.ui.lbl_scrapers_elMundo_tech_result.text()))
+
+        self.ui.lbl_scrapers_elPais_health_result.setNum(scr.count_files('elPais', 'salud'))
+        self.ui.lbl_scrapers_elPais_science_result.setNum(scr.count_files('elPais', 'ciencia'))
+        self.ui.lbl_scrapers_elPais_tech_result.setNum(scr.count_files('elPais', 'tecnologia'))
+        self.ui.lbl_scrapers_elPais_total_result.setNum(
+            int(self.ui.lbl_scrapers_elPais_health_result.text()) +
+            int(self.ui.lbl_scrapers_elPais_science_result.text()) +
+            int(self.ui.lbl_scrapers_elPais_tech_result.text()))
 
 
 def run():
--- scrapers/scrapers.py
+++ scrapers/scrapers.py
@@ -17,7 +17,7 @@ def article_exists(old, new):
 
 
 def store_data(directory, category, content):
-    path = "../data/" + directory + "/" + category + "/"
+    path = "./data/" + directory + "/" + category + "/"
     dates = []
 
     exist = False
@@ -35,164 +35,172 @@ def store_data(directory, category, content):
 
     print(exist)
     if not exist:
-        path = "../data/" + directory + "/" + category + "/" + category + "." + content['date'] + '.{0:0>3}'.format(
+        path = "./data/" + directory + "/" + category + "/" + category + "." + content['date'] + '.{0:0>3}'.format(
             a_id) + ".json"
         print('> Writing file: ', path)
         with open(path, 'w') as f:
             json.dump(content, f, indent=4, ensure_ascii=False)
 
 
-def scraper_elMundo(category):
-    if category is "ciencia" or category is "salud":
-        url = "https://www.elmundo.es/ciencia-y-salud/" + category
-    else:
-        url = "https://www.elmundo.es/tecnologia"
+def scraper_elMundo(categories):
+    for category in categories:
+        if category is "ciencia" or category is "salud":
+            url = "https://www.elmundo.es/ciencia-y-salud/" + category
+        else:
+            url = "https://www.elmundo.es/tecnologia"
 
-    page = requests.get(url + ".html")
-    soup = BeautifulSoup(page.content, features='html.parser')
+        page = requests.get(url + ".html")
+        soup = BeautifulSoup(page.content, features='html.parser')
 
-    for article in soup.find_all('article'):
-        for a in article.find_all('a'):
-            hrefs = a['href'].split('/')
-            if 'ancla_comentarios' not in a['href'].split('#') and 'promociones' not in hrefs and 'album' not in hrefs:
-                page_article = requests.get(a['href'])
-                soup_article = BeautifulSoup(page_article.content, features='html.parser')
+        for article in soup.find_all('article'):
+            for a in article.find_all('a'):
+                hrefs = a['href'].split('/')
+                if 'ancla_comentarios' not in a['href'].split(
+                        '#') and 'promociones' not in hrefs and 'album' not in hrefs:
+                    page_article = requests.get(a['href'])
+                    soup_article = BeautifulSoup(page_article.content, features='html.parser')
 
-                article_title = soup_article.find('h1').get_text()
-                article_subtitle = soup_article.find('p', class_='ue-c-article__standfirst').get_text()
-                article_author = soup_article.find('div', class_='ue-c-article__byline-name').get_text()
-                article_datetime = soup_article.find('time')['datetime']
-                article_date = datetime.strptime(article_datetime, "%Y-%m-%dT%H:%M:%SZ").strftime('%Y-%m-%d')
-                article_time = datetime.strptime(article_datetime, "%Y-%m-%dT%H:%M:%SZ").strftime('%H:%M')
-                article_content = ""
-                content = soup_article.find('div', class_='ue-l-article__body ue-c-article__body')
-                for p_tag in content.find_all('p', recursive=False):
-                    article_content += p_tag.get_text()
+                    article_title = soup_article.find('h1').get_text()
+                    article_subtitle = soup_article.find('p', class_='ue-c-article__standfirst').get_text()
+                    article_author = soup_article.find('div', class_='ue-c-article__byline-name').get_text()
+                    article_datetime = soup_article.find('time')['datetime']
+                    article_date = datetime.strptime(article_datetime, "%Y-%m-%dT%H:%M:%SZ").strftime('%Y-%m-%d')
+                    article_time = datetime.strptime(article_datetime, "%Y-%m-%dT%H:%M:%SZ").strftime('%H:%M')
+                    article_content = ""
+                    content = soup_article.find('div', class_='ue-l-article__body ue-c-article__body')
+                    for p_tag in content.find_all('p', recursive=False):
+                        article_content += p_tag.get_text()
 
-                article_json = {
-                    'title': article_title,
-                    'subtitle': article_subtitle,
-                    'author': article_author,
-                    'date': article_date,
-                    'time': article_time,
-                    'content': article_content,
-                    'processed': None
-                }
-                store_data('elMundo', category, article_json)
-
-
-def scraper_elPais(category):
-    # switch category
-    if category is "tecnologia" or category is "ciencia":
-        url = "https://elpais.com/" + category + "/"
-    else:
-        url = "https://elpais.com/noticias/" + category + "/"
-
-    if category is "sanidad":
-        category = "salud"
-
-    # get page response
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, features='html.parser')
-
-    # get articles from category page
-    for article in soup.find_all('article', class_='story_card'):
-        for a in article.find_all('a'):
-            hrefs = a['href'].split('/')
-            if "autor" not in hrefs and \
-                    "hemeroteca" not in hrefs and \
-                    "actualidad" not in hrefs and \
-                    "https:" not in hrefs and \
-                    "elpais" not in hrefs:  # remove video links
-                print(a['href'])
-                page_article = requests.get("https://elpais.com" + a['href'])
-                soup_article = BeautifulSoup(page_article.content, features='html.parser')
-
-                article_content = ""
-                content = soup_article.find('div', class_='article_body')
-
-                article_title = soup_article.find('h1').get_text()
-                article_subtitle = soup_article.find('h2').get_text()
-                try:
-                    article_author = soup_article.find('a', class_='color_black').get_text()
-                except:
-                    article_author = soup_article.find('span',
-                                                       class_='margin_bottom uppercase flex align_items_center margin_right').get_text()
-                article_date = soup_article.find('div', class_='place_and_time').find('a')['href'].split('/')[-2]
-                article_time = \
-                    soup_article.find('div', class_='place_and_time').find('a', recursive=False).get_text().split('-')[
-                        -1].strip()  # todo: fix time error UTC+1
-
-                for p_tag in content.find_all('p', recursive=False):
-                    article_content = p_tag.get_text()
-
-                article_json = {
-                    'title': article_title,
-                    'subtitle': article_subtitle,
-                    'author': article_author,
-                    'date': article_date,
-                    'time': article_time,
-                    'content': article_content,
-                    'processed': None
-                }
-                store_data('elPais', category, article_json)
+                    article_json = {
+                        'title': article_title,
+                        'subtitle': article_subtitle,
+                        'author': article_author,
+                        'date': article_date,
+                        'time': article_time,
+                        'content': article_content,
+                        'processed': None
+                    }
+                    store_data('elMundo', category, article_json)
 
 
-def scraper_20minutos(category):
-    # switch category
-    url = "https://20minutos.es/" + category + "/"
+def scraper_elPais(categories):
+    for category in categories:
+        # switch category
+        if category is "tecnologia" or category is "ciencia":
+            url = "https://elpais.com/" + category + "/"
+        else:
+            category = "sanidad"
+            url = "https://elpais.com/noticias/" + category + "/"
 
-    # get page response
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, features='html.parser')
+        if category is "sanidad":
+            category = "salud"
 
-    links = []
+        # get page response
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, features='html.parser')
 
-    # get articles from category page
-    for article in soup.find_all('article', class_='media'):
-        for a in article.find_all('a'):
-            if a['href'] not in links and \
-                    "noticia" in a['href'] and \
-                    "#" not in a['href'] and \
-                    "4220544" not in a['href'] and \
-                    a['href'][0] is not "/":
-                links.append(a['href'])
-                page_article = requests.get(a['href'])
-                soup_article = BeautifulSoup(page_article.content, features='html.parser')
+        # get articles from category page
+        for article in soup.find_all('article', class_='story_card'):
+            for a in article.find_all('a'):
+                hrefs = a['href'].split('/')
+                if "autor" not in hrefs and \
+                        "hemeroteca" not in hrefs and \
+                        "actualidad" not in hrefs and \
+                        "https:" not in hrefs and \
+                        "elpais" not in hrefs:  # remove video links
+                    page_article = requests.get("https://elpais.com" + a['href'])
+                    soup_article = BeautifulSoup(page_article.content, features='html.parser')
 
-                article_title = soup_article.find_all('h1')[-1].get_text()
-                try:
-                    article_subtitle = soup_article.find('div', class_='article-intro').find('li').get_text()
-                except:
-                    article_subtitle = soup_article.find('div', class_='article-intro').get_text()
-                article_author = soup_article.find('span', class_='article-author').find('strong').get_text()
+                    article_content = ""
+                    content = soup_article.find('div', class_='article_body')
 
-                article_datetime = soup_article.find('span', class_='article-date').find('a').get_text()
-                article_date = datetime.strptime(article_datetime, "%d.%m.%Y - %H:%Mh").strftime('%Y-%m-%d')
-                article_time = datetime.strptime(article_datetime, "%d.%m.%Y - %H:%Mh").strftime('%H:%M')
+                    article_title = soup_article.find('h1').get_text()
+                    article_subtitle = soup_article.find('h2').get_text()
+                    try:
+                        article_author = soup_article.find('a', class_='color_black').get_text()
+                    except:
+                        try:
+                            article_author = soup_article.find('span', class_='margin_bottom uppercase flex align_items_center margin_right').get_text()
+                        except:
+                            article_author = None
 
-                article_content = ""
-                content = soup_article.find('div', class_='article-text')
-                for p_tag in content.find_all('p', class_='paragraph', recursive=False):
-                    article_content += p_tag.get_text()
+                    try:
+                        article_date = soup_article.find('div', class_='place_and_time').find('a')['href'].split('/')[-2]
+                        article_time = soup_article.find('div', class_='place_and_time').find('a', recursive=False).get_text().split('-')[-1].strip()  # todo: fix time error UTC+1
+                    except:
+                        article_date = soup_article.find('a', class_='a_ti')['href'].split('/')[-2]
+                        article_time = None
 
-                article_json = {
-                    'title': article_title,
-                    'subtitle': article_subtitle,
-                    'author': article_author,
-                    'date': article_date,
-                    'time': article_time,
-                    'content': article_content,
-                    'processed': None
-                }
-                store_data('20Minutos', category, article_json)
+                    for p_tag in content.find_all('p', recursive=False):
+                        article_content = p_tag.get_text()
+
+                    article_json = {
+                        'title': article_title,
+                        'subtitle': article_subtitle,
+                        'author': article_author,
+                        'date': article_date,
+                        'time': article_time,
+                        'content': article_content,
+                        'processed': None
+                    }
+                    store_data('elPais', category, article_json)
 
 
-if __name__ == '__main__':
-    # scraper_elMundo('ciencia')
-    # scraper_elPais('ciencia')
-    # scraper_elPais('sanidad')
-    # scraper_elPais('tecnologia')
-    # scraper_20minutos('salud')
-    # scraper_20minutos('ciencia')
-    scraper_20minutos('tecnologia')
+def scraper_20minutos(categories):
+    for category in categories:
+        # switch category
+        url = "https://20minutos.es/" + category + "/"
+
+        # get page response
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, features='html.parser')
+
+        links = []
+
+        # get articles from category page
+        for article in soup.find_all('article', class_='media'):
+            for a in article.find_all('a'):
+                if a['href'] not in links and \
+                        "noticia" in a['href'] and \
+                        "#" not in a['href'] and \
+                        "4220544" not in a['href'] and \
+                        "4222535" not in a['href'] and \
+                        a['href'][0] is not "/" and \
+                        a['href'][0] is not "2":
+                    links.append(a['href'])
+                    page_article = requests.get(a['href'])
+                    soup_article = BeautifulSoup(page_article.content, features='html.parser')
+
+                    article_title = soup_article.find_all('h1')[-1].get_text()
+                    try:
+                        article_subtitle = soup_article.find('div', class_='article-intro').find('li').get_text()
+                    except:
+                        try:
+                            article_subtitle = soup_article.find('div', class_='article-intro').get_text()
+                        except:
+                            article_subtitle = None
+                    article_author = soup_article.find('span', class_='article-author').find('strong').get_text()
+
+                    article_datetime = soup_article.find('span', class_='article-date').find('a').get_text()
+                    article_date = datetime.strptime(article_datetime, "%d.%m.%Y - %H:%Mh").strftime('%Y-%m-%d')
+                    article_time = datetime.strptime(article_datetime, "%d.%m.%Y - %H:%Mh").strftime('%H:%M')
+
+                    article_content = ""
+                    content = soup_article.find('div', class_='article-text')
+                    for p_tag in content.find_all('p', class_='paragraph', recursive=False):
+                        article_content += p_tag.get_text()
+
+                    article_json = {
+                        'title': article_title,
+                        'subtitle': article_subtitle,
+                        'author': article_author,
+                        'date': article_date,
+                        'time': article_time,
+                        'content': article_content,
+                        'processed': None
+                    }
+                    store_data('20Minutos', category, article_json)
+
+
+def count_files(source, category):
+    return len(os.listdir('./data/' + source + '/' + category))
