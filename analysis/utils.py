3cf3895d19a52ac2d589c6f4c5540eb861b76e73
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-04-10 19:22:44 +0200

    unnecesary functions

--- analysis/utils.py
+++ analysis/utils.py
@@ -110,7 +110,7 @@ def compute_tf_idf(df_tf, df_idf):
     return df_tf_idf
 
 
-def cosine_measure(df_tf_idf, df_tf_idf_query):
+def cosine_formula(df_tf_idf, df_tf_idf_query):
     numerator = sum(df_tf_idf['values'] * df_tf_idf_query['values'])
 
     sum_file = 0
@@ -120,56 +120,13 @@ def cosine_measure(df_tf_idf, df_tf_idf_query):
         sum_query = sum_query + pow(df_tf_idf_query['values'][j], 2)
 
     denominator = math.sqrt(sum_file) * math.sqrt(sum_query)
-    if denominator == 0:
+    if denominator is 0:
         return 0
     else:
         return numerator / denominator
 
 
-def total_result(sources, categories, total_idf, query_tf_idf):
-    stemmed_tokens = []
-    df_idf1 = pd.DataFrame(columns=['words', 'values'])
-
-    # remove unnecessary tokens
-    for i in range(len(query_tf_idf)):
-        if query_tf_idf['words'][i] in total_idf['words'].values:
-            df_idf1 = df_idf1.append(total_idf[total_idf['words'] == query_tf_idf['words'][i]])
-        else:
-            df_idf1.loc[i] = [query_tf_idf['words'][i], 0]
-    df_idf1.reset_index(inplace=True)
-    del df_idf1['index']
-    # print(df_idf1)
-
-    for source in sources:
-        for category in categories:
-            dir_path = './data/' + source + '/' + category + '/'
-            for file in os.listdir(dir_path):
-                filepath = dir_path + file
-                with open(filepath, 'r') as f:
-                    js = json.load(f)
-                    stemmed_tokens.append(js['processed'])
-                    f.close()
-
-    for article_token in stemmed_tokens:
-        tf = compute_tf(article_token)
-        df_tf = pd.DataFrame(columns=['words', 'values'])
-
-        # remove unnecessary tokens
-        for i in range(len(query_tf_idf)):
-            if query_tf_idf['words'][i] in tf['words'].values:
-                df_tf = df_tf.append(tf[tf['words'] == query_tf_idf['words'][i]])
-            else:
-                df_tf.loc[i] = [query_tf_idf['words'][i], 0]
-        df_tf.reset_index(inplace=True)
-        del df_tf['index']
-
-        token_tf_idf = compute_tf_idf(df_tf=df_tf, df_idf=df_idf1)
-
-        sim = cosine_measure(df_tf_idf=token_tf_idf, df_tf_idf_query=query_tf_idf)
-    return sim
-
-
-def total_result_wm(sources, categories, total_idf, query_tf_idf, top):
+def query_similarity(sources, categories, total_idf, query_tf_idf, top):
     stemmed_tokens = []
     json_files = []
     df_idf1 = pd.DataFrame(columns=['words', 'values'])
@@ -184,6 +141,7 @@ def total_result_wm(sources, categories, total_idf, query_tf_idf, top):
     df_idf1.reset_index(inplace=True)
     del df_idf1['index']
 
+    # save all stemmed tokens and filename
     for source in sources:
         for category in categories:
             dir_path = './data/' + source + '/' + category + '/'
@@ -209,8 +167,7 @@ def total_result_wm(sources, categories, total_idf, query_tf_idf, top):
         del df_tf['index']
 
         token_tf_idf = compute_tf_idf(df_tf=df_tf, df_idf=df_idf1)
-
-        sim = cosine_measure(df_tf_idf=token_tf_idf, df_tf_idf_query=query_tf_idf)
+        sim = cosine_formula(df_tf_idf=token_tf_idf, df_tf_idf_query=query_tf_idf)
 
         if len(df_sim) < top:
             df_sim.loc[len(df_sim)] = [json_files[s], sim]
@@ -218,6 +175,8 @@ def total_result_wm(sources, categories, total_idf, query_tf_idf, top):
             if any(z < sim for z in df_sim['similarity'].values):
                 pos = df_sim[df_sim['similarity'] == df_sim['similarity'].min()].index[0]
                 df_sim.loc[pos] = [json_files[s], sim]
+
+    # order by DES and reset index
     df_sim = df_sim.sort_values('similarity', ascending=False)
     df_sim.reset_index(inplace=True)
     del df_sim['index']
@@ -234,15 +193,3 @@ def get_query_tf_idf(query):
 
 def total_idf(sources, categories):
     return compute_idf_tokens(sources, categories)
-
-# if __name__ == '__main__':
-    # compute_idf_tokens(['20Minutos', 'elMundo', 'elPais'], ['ciencia', 'salud', 'tecnologia'])
-
-    # query = "El límite en el reenvío de los mensajes a solo una vez por grupo llega después de que la aplicación ya restringiera la cantidad "
-    # tf = compute_tf(pre_processing(query))
-    # idf = compute_idf(pre_processing(query))
-    # query_tf_idf = compute_tf_idf(df_tf=tf, df_idf=idf)
-
-    # total_idf = compute_idf_tokens(['elPais'], ['tecnologia'])
-    #
-    # print(total_result_wm(['elPais'], ['tecnologia'], total_idf, query_tf_idf, top=5))
